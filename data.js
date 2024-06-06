522698bd4fa404678926bf9594c10a9b65ae991a
Gaël Grandval <155240126+Grd-gael@users.noreply.github.com>
2024-06-06 15:13:05 +0200

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,7 @@
 var data = [
     {"class-id":'animal', "analogie": "un animal", "valeur": "un renard", 'explication': 'parce qu\'en plus d\'être mon animal favori, cet animal se caractérise par son indépendance, sa malice et son adaptabilité, ce qui me correspond plutôt bien.  De plus, son pelage orangé et blanc rassemble 2 couleurs que je trouve magnifique, c\'est ce qui fait de lui un animal élégant.'},
 
-    { "class-id":'saison', "analogie": "une saison", "valeur": "l'été", 'explication': 'parce que cette saison repésente les vacances, le soleil, les festivals. <br> Cette période de l\'année, sans stress, est remplie de souvenirs, c\'est à ce moment qu\'on retrouve ses amis ou sa famille dans des endroits où la tranquillité et le bonheur règne comme la plage.'},
+    { "class-id":'saison', "analogie": "une saison", "valeur": "l'été", 'explication': 'parce que cette saison repésente les vacances, le soleil, les festivals. <br> Cette période de l\'année, sans stress, est remplie de souvenirs, c\'est à ce moment qu\'on retrouve ses amis ou sa famille dans des endroits où la tranquillité et le bonheur règnent comme la plage.'},
 
     { "class-id":'moment', "analogie": "un moment de la journée", "valeur": "le coucher du soleil", 'explication': 'parce que c\'est un paysage que je ne me lasse jamais d\'aller voir. Que ce soit au bord de la mer, à la campagne ou en ville, le soleil éclaire tout l\'environnement d\'une couleur orangée qui créer immédiatement une ambiance inspirant le calme et la paix.'},
 
@@ -13,6 +13,6 @@ var data = [
 
     { "class-id":'sentiment', "analogie": "un sentiment", "valeur": "la nostalgie", 'explication': 'parce que ce sentiment me rapelle tous les moments joyeux et inoubliables vécus lors de ma jeunesse. Contrairement à la plupart des personnes, j\'affectionne tout particulièrement lorsque ces moments de vies innocents reviennent à nous, même si ils viennent souvent avec la tristesse de ne jamais pouvoir les revivre...'}, 
 
-    { "class-id":'groupe', "analogie": "un groupe", "valeur": "Coldplay", 'explication': 'parce que ce groupe a la capacité de créer des musiques qui me transporte et qui me font ressentir toutes sortes d\'émotions. Leurs mélodies captivantes et leurs paroles poétiques nous plongent dans une expérience auditivive magnifique ce qui explique mon attachement profond à ce groupes et leur compositions. '},
+    { "class-id":'groupe', "analogie": "un groupe", "valeur": "Coldplay", 'explication': 'parce que ce groupe a la capacité de créer des musiques qui me transportent et qui me font ressentir toutes sortes d\'émotions. Leurs mélodies captivantes et leurs paroles poétiques nous plongent dans une expérience auditivive magnifique ce qui explique mon attachement profond à ce groupe et leurs compositions. '},
 
     { "class-id":'jeu', "analogie": "une licence de jeu", "valeur": "The legend of Zelda", 'explication': 'parce que cette licence a bercé mon enfance grâce à ses inombrables jeux, tous différents mais ayant le même univers magnifique et travaillé dont je suis un grand fan. Les musiques, les graphismes et les personnages attachants font de ces jeux des expériences uniques que je rêverai de pouvoir redécouvrir pour la première fois.' }]
